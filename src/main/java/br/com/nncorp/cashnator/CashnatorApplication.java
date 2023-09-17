f7328dd79bb00f71d9b8e5afcf15a833e9b171a2
NatanLisboa <natanflisboa1@gmail.com>
2023-09-17 00:42:58 -0300

    Initial commit

---
+++ src/main/java/br/com/nncorp/cashnator/CashnatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.nncorp.cashnator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashnatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashnatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nncorp/cashnator/CashnatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nncorp.cashnator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashnatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
